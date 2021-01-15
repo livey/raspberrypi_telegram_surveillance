01471342c1de47cf3573dda01b3b97470ac9b3f5
Fuwei Li <liveysmile@gmail.com>
2021-01-14 20:36:55 -0800

    use global surveillance enable

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import telebot
 import time 
 from pi_camera import pi_camera
 from audio import audio_input 
-token = 'your token'
-
+token = 'your telegram bot token'
 bot = telebot.TeleBot(token = token)
-recent_chat_id = your chat id 
+recent_chat_id =  your-chat-id 
 
-motion_detect_enabled = True 
+surveillance_enabled = True 
 camera = pi_camera(video_res = (1296, 972), framerate= 8) #(1296, 730) (640, 480) (1920, 1080) (1296, 972)
 audio = audio_input(rate = 44100, chunck = 8192, device_index=1, time_record = 10, save_dirc='./audio_clips')
 
 @bot.message_handler(commands=['start'])
 def start_surveillance(message):
-    global motion_detect_enabled 
+    global surveillance_enabled 
     global recent_chat_id 
     print('surveillance enabled')
     recent_chat_id = message.chat.id
-    motion_detect_enabled = True
+    surveillance_enabled = True
     bot.reply_to(message, 'start watching') 
 
 @bot.message_handler(commands=['photo'])
@@ -47,11 +46,11 @@ def send_video_clips(message):
 
 @bot.message_handler(commands=['stop'])
 def stop_surveillance(message):
-    global motion_detect_enabled
+    global surveillance_enabled
     global recent_chat_id 
     print('stopping the surveillance')
     recent_chat_id = message.chat.id
-    motion_detect_enabled = False
+    surveillance_enabled = False
     bot.reply_to(message, 'stop watch')
 
 @bot.message_handler(commands=['audio'])
@@ -67,10 +66,11 @@ def send_audio_clips(message):
         bot.send_message(recent_chat_id, 'bot is busy, wait for a while')
 
 def send_audio_event():
+    global surveillance_enabled 
     global recent_chat_id 
     while True:
         time.sleep(0.01)
-        if audio.voice_detected():
+        if audio.voice_detected() and surveillance_enabled:
             print('voice detected at time: ' +  time.strftime("%Y%m%d-%H%M%S"))
             file_name = audio.cap_voice_event()
             bot.send_message(recent_chat_id, 'voice detected')
@@ -87,11 +87,11 @@ def periodic_cap_image():
         time.sleep(600)
 
 def send_motion_event():
-    global motion_detect_enabled
+    global surveillance_enabled
     global recent_chat_id 
     while True:
         time.sleep(0.01)
-        if camera.motion_detected() and motion_detect_enabled:
+        if camera.motion_detected() and surveillance_enabled:
             print('motion detected at time ' + time.strftime("%Y%m%d-%H%M%S"))
             file_name = camera.cap_motion_event()
             bot.send_message(recent_chat_id, 'motion detected')
